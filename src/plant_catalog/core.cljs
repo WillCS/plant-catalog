8c545ad95a3896690dd4bef04018e34a4626183c
WillCS <wcstibbards@gmail.com>
2022-03-18 21:31:40 +1000

    Restarted project, doing a single page app with a backend instead of server side rendering.

--- src/plant_catalog/core.cljs
+++ src/plant_catalog/core.cljs
@@ -1,39 +1,26 @@
 (ns plant-catalog.core
-  (:require ["express" :as express]
-            [reagent.dom.server :as reagent-server]))
+  (:require [reagent.core :as reagent :refer [atom]]))
 
-(defonce server (atom nil))
+;; define your app data so that it doesn't get over-written on reload
 
-(defn simple-component []
+(defonce app-state (atom {:text "Hello world!"}))
+
+(defn hello-world []
   [:div
-   [:p "Here's some text"]
-   [:p.testclass
-    "Here's some more text. " [:string "it's bold."]
-    [:span {:style {:color "red"}} " and red text"] " normal here though."]])
+   [:h1 (:text @app-state)]
+   [:h3 "Edit this and watch it change!"]])
 
-(defn render-component []
-  (reagent-server/render-to-string(simple-component)))
+(defn start []
+  (reagent/render-component [hello-world]
+                            (. js/document (getElementById "app"))))
 
-(defn routes [^js app]
-  (.get app "/" (fn [req res] (.send res (render-component)))))
+(defn ^:export init []
+  ;; init is called ONCE when the page loads
+  ;; this is called in the index.html and must be exported
+  ;; so it is available even in :advanced release builds
+  (start))
 
-(defn start-server []
-  (let [app (express)
-        prod? (= (.get app "env") "production")
-        port (if (nil? (.-PORT (.-env js/process)))
-               3000
-               (int (.-PORT (.-env js/process))))]
-    (when prod? (.set app "trust proxy" 1))
-    (.use app "/assets" (.static express "assets"))
-    (routes app)
-    (.listen app port (fn [] (prn "Listening")))))
-
-(defn start! []
-  (reset! server (start-server)))
-
-(defn stop! []
-  (.close @server)
-  (reset! server nil))
-
-(defn main [& args]
-  (start!))
+(defn stop []
+  ;; stop is called before any code is reloaded
+  ;; this is controlled by :before-load in the config
+  (js/console.log "stop"))
